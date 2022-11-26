3e4bd0eced48cfd21b9d6fb1c41a8b51bd93053e
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-11-27 01:48:23 +0530

    delet buyers ans selers

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ async function run() {
       res.send(result);
     });
 
-
     //all  products **********************************
     // all products get
     app.get("/allProducts/:id", async (req, res) => {
@@ -48,22 +47,22 @@ async function run() {
       const result = await allProducts.find(query).toArray();
       res.send(result);
     });
-    
-    //my products get with email 
+
+    //my products get with email
     app.get("/mypro", async (req, res) => {
       const emails = req.query.email;
-      const userinformation = {emali : emails};
+      const userinformation = { emali: emails };
       const result = await allProducts.find(userinformation).toArray();
       res.send(result);
     });
 
-    app.delete('/mypro/:id' , async(req , res) =>{
-      const id = req.params.id
+    app.delete("/mypro/:id", async (req, res) => {
+      const id = req.params.id;
       console.log(id);
-      const query = {_id : ObjectId(id)}
+      const query = { _id: ObjectId(id) };
       const result = await allProducts.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // buy product   get
     app.get("/books/:id", async (req, res) => {
@@ -73,13 +72,29 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/allProduct' , async(req , res)=>{
+    app.post("/allProduct", async (req, res) => {
       const info = req.body;
       console.log(info);
-      const result =await allProducts.insertOne(info);
-      res.send(result)
-    })
+      const result = await allProducts.insertOne(info);
+      res.send(result);
+    });
 
+    //update book in pmant sistem is done
+    app.put("/advertise/:id", async (req, res) => {
+      const ids = req.params.id;
+      console.log(ids);
+
+      const filter = { _id: ObjectId(ids) };
+
+      const option = { upsert: true };
+      const updateUser = {
+        $set: {
+          advertise: "done",
+        },
+      };
+      const result = await allProducts.updateOne(filter, updateUser, option);
+      res.send(result);
+    });
 
     // book **********************************
     //  post
@@ -89,13 +104,39 @@ async function run() {
       res.send(result);
     });
     //get book
-    app.get('/booking', async(req , res)=>{
-      const email =req.query.email
-      console.log(email);
-      const query = {productemail : email}
-      const baiours =await bookcollictions.find(query).toArray();
-      res.send(baiours)
-    })
+    app.get("/booking", async (req, res) => {
+      const email = req.query.email;
+      const query = { productemail: email };
+      const baiours = await bookcollictions.find(query).toArray();
+      res.send(baiours);
+    });
+    //my order
+    app.get("/myorders", async (req, res) => {
+      const email = req.query.email;
+      const query = { useremail: email };
+      const baiour = await bookcollictions.find(query).toArray();
+      res.send(baiour);
+    });
+    //update book in pmant sistem is done
+    app.put("/bokingd/:id", async (req, res) => {
+      const ids = req.params.id;
+      console.log(ids);
+
+      const filter = { _id: ObjectId(ids) };
+
+      const option = { upsert: true };
+      const updateUser = {
+        $set: {
+          pmant: "done",
+        },
+      };
+      const result = await bookcollictions.updateOne(
+        filter,
+        updateUser,
+        option
+      );
+      res.send(result);
+    });
 
     // user informaito sate in database***************************
     //post
@@ -111,11 +152,31 @@ async function run() {
       const result = await userscollictions.find(userinformation).toArray();
       res.send(result);
     });
+    //all seler get
+    app.get("/allseler", async (req, res) => {
+      const query = { roll: "Seller account" };
+      const result = await userscollictions.find(query).toArray();
+      res.send(result);
+    });
+    //all Buyers account get
+    app.get("/allbuyers", async (req, res) => {
+      const query = { roll: "Buyers account" };
+      const result = await userscollictions.find(query).toArray();
+      res.send(result);
+    });
+    //seler delete
+    app.delete("/seler/:id", async (req, res) => {
+      const ids = req.params.id;
+      console.log(ids);
+      const query = { _id: ObjectId(ids) };
+      const result = await userscollictions.deleteOne(query);
+      res.send(result);
+    });
 
     // pment explore**********************************
     app.post("/create-payment-intent", async (req, res) => {
-      const p = req.body;
-      const amount = parseInt(p.Price) * 100;
+      const price = req.body;
+      const amount = parseInt(price.price) * 100;
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "inr",
